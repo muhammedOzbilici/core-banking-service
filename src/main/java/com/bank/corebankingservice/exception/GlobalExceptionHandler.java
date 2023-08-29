3c4ab9c3931e8b57ed85deb96d36dc9504e04dcb
muhammed.ozbilici <muhammed.ozbilici88@gmail.com>
2023-08-29 11:45:34 +0300

    first commit

--- src/main/java/com/bank/corebankingservice/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bank/corebankingservice/exception/GlobalExceptionHandler.java
@@ -13,33 +13,4 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(accountNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-//    @ExceptionHandler(value = InvalidCurrencyException.class)
-//    public ResponseEntity<?> invalidCurrencyException(InvalidCurrencyException invalidCurrencyException) {
-//        return new ResponseEntity<>(invalidCurrencyException.getMessage(), HttpStatus.BAD_REQUEST);
-//    }
-//
-//    @ExceptionHandler(value = InsufficientFundException.class)
-//    public ResponseEntity<?> insufficientFundException(InsufficientFundException insufficientFundException) {
-//        return new ResponseEntity<>(insufficientFundException.getMessage(), HttpStatus.FORBIDDEN);
-//    }
-//
-//    @ExceptionHandler(JsonMappingException.class)
-//    public ResponseEntity<?> handleConverterErrors(JsonMappingException exception) {
-//        return new ResponseEntity<>(exception.getMessage(), HttpStatus.BAD_REQUEST);
-//    }
-//
-//    @ExceptionHandler(HttpMessageNotReadableException.class)
-//    public ResponseEntity<?> handleConverterErrors(HttpMessageNotReadableException exception) {
-//        var message = exception.getMessage() != null &&
-//                !exception.getMessage().isEmpty() &&
-//                exception.getMessage().contains("com.tuum.bankassignment.entity.Currency") ?
-//                "Invalid Currency" : "Bad request";
-//        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
-//    }
-//
-//    @ExceptionHandler(value = Exception.class)
-//    public ResponseEntity<?> databaseConnectionFailsException(Exception exception) {
-//        logger.info(exception.getMessage());
-//        return new ResponseEntity<>("There is a problem", HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
 }
